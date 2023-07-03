822ce88b31305beef085aa75c9aeeae9f46fc071
NKV <vi-nguyen@sens-vn.com>
2023-07-03 15:33:45 +0700

    trigger

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 import express from "express";
-import dotenv from 'dotenv';
-dotenv.config({})
+import dotenv from "dotenv";
+dotenv.config({});
 
 import mongoose from "mongoose";
 const app = express();
 mongoose
-    .connect(
-        "mongodb://myusername:mypassword@172.20.0.2:27017/?authSource=admin"
-    )
-    .then(() => {
-        console.log(`[*] mongoDB connected`);
-    })
-    .catch((e) => {
-        console.log(`Err: ${JSON.stringify(e)}`);
-    });
+  .connect("mongodb://myusername:mypassword@172.20.0.2:27017/?authSource=admin")
+  .then(() => {
+    console.log(`[*] mongoDB connected`);
+  })
+  .catch((e) => {
+    console.log(`Err: ${JSON.stringify(e)}`);
+  });
 app.set("PORT", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-    res.send(
-        `<h1>Just a simple project to learn docker - devops with nodejs, express 123<h1>`
-    );
+  res.send(
+    `<h1>Just a simple project to learn docker - devops with nodejs, express<h1>`
+  );
 });
 app.listen(app.get("PORT"), () => {
-    console.log(`[*] Server is running on port: ${app.get("PORT")}`);
+  console.log(`[*] Server is running on port: ${app.get("PORT")}`);
 });
